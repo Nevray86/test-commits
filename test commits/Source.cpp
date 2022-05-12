125221d36b686413af67e6c98e1a572d1f397366
Андрей <nevraev1109@mail.ru>
2022-05-12 17:16:33 +0500

    Add project files.

---
+++ test commits/Source.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+#include<algorithm>
+using namespace std;
+
+
+
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	srand(time(NULL));
+	
+	cout << "commit 1";
+
+
+	return 0;
+}
